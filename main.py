0fc1804a056233ab2e324086654a872a81f45151
powercxx <panjiayi@sjtu.edu.cn>
2021-12-19 11:24:04 -0500

    Format

--- main.py
+++ main.py
@@ -7,32 +7,34 @@ import time
 
 #########################
 
+
 def main(screenshot=False):
     # initialize PyBullet
     connect(use_gui=True)
     # load robot and obstacle resources
-    robots, obstacles = load_env('pr2doorway.json')
+    robots, obstacles = load_env('pr2maze.json')
 
     # define active DoFs
-    base_joints = [joint_from_name(robots['pr2'], name) for name in PR2_GROUPS['base']]
+    base_joints = [joint_from_name(robots['pr2'], name)
+                   for name in PR2_GROUPS['base']]
 
-    collision_fn = get_collision_fn_PR2(robots['pr2'], base_joints, list(obstacles.values()))
+    collision_fn = get_collision_fn_PR2(
+        robots['pr2'], base_joints, list(obstacles.values()))
     # Example use of collision checking
     # print("Robot colliding? ", collision_fn((0.5, -1.3, -np.pi/2)))
 
     # Example use of setting body poses
     # set_pose(obstacles['ikeatable6'], ((0, 0, 0), (1, 0, 0, 0)))
 
-    # Example of draw 
+    # Example of draw
     # draw_sphere_marker((0, 0, 1), 0.1, (1, 0, 0, 1))
-    
+
     start_config = tuple(get_joint_positions(robots['pr2'], base_joints))
     goal_config = (2.6, -1.3, -np.pi/2)
     path = []
     start_time = time.time()
     ### YOUR CODE HERE ###
-    
-    
+
     ######################
     print("Planner run time: ", time.time() - start_time)
     # Execute planned path
@@ -40,6 +42,7 @@ def main(screenshot=False):
     # Keep graphics window opened
     wait_if_gui()
     disconnect()
+
 
 if __name__ == '__main__':
     main()

2dcfd341baf952f819e33bd43ed2b4be0a36de87
powercxx <panjiayi@sjtu.edu.cn>
2021-12-19 14:52:31 -0500

    Path Gen

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from utils import get_collision_fn_PR2, load_env, execute_trajectory, draw_spher
 from pybullet_tools.utils import connect, disconnect, get_joint_positions, wait_if_gui, set_joint_positions, joint_from_name, get_link_pose, link_from_name
 from pybullet_tools.pr2_utils import PR2_GROUPS
 import time
+import model
 ### YOUR IMPORTS HERE ###
 
 #########################
@@ -31,14 +32,15 @@ def main(screenshot=False):
 
     start_config = tuple(get_joint_positions(robots['pr2'], base_joints))
     goal_config = (2.6, -1.3, -np.pi/2)
-    path = []
+    path = [[1, 10, 10]]
     start_time = time.time()
     ### YOUR CODE HERE ###
 
     ######################
     print("Planner run time: ", time.time() - start_time)
     # Execute planned path
-    execute_trajectory(robots['pr2'], base_joints, path, sleep=0.2)
+    # execute_trajectory(robots['pr2'], base_joints, path, sleep=0.2)
+    execute_trajectory(robots['pr2'], base_joints, model.Path, sleep=0.2)
     # Keep graphics window opened
     wait_if_gui()
     disconnect()
---
+++ pathGen.py
@@ -0,0 +1,39 @@
+# %%
+import numpy as np
+
+# %%
+KeyPos = [[-6.5, 4], [-6.5, -7], [1, -7],
+          [1, 0.5], [6.5, 0.5], [6.5, 4.5], [13, 4.5]]
+
+# %%
+# Path with Noise
+STEP_SIZE = 0.25
+NOISE_SCALE = 0.1
+Path = []
+Noise_2 = [0, 0]
+for i in range(len(KeyPos)-1):
+    s = KeyPos[i]
+    e = KeyPos[i+1]
+    isY = True  # check if going in y direction
+    if (s[1]-e[1] == 0):
+        isY = False
+    idx = 1 if isY else 0
+    Negidx = 0 if isY else 1
+    dL = e[idx] - s[idx]
+    n = int(np.ceil(np.abs(dL)/STEP_SIZE))
+    pos = 1 if dL > 0 else -1  # if going in positive direction
+    for i in range(n):
+        step = s.copy()
+        step.append(0.05)  # height
+        step[idx] += i * STEP_SIZE * pos
+        if (i-4) < n:
+            if (i % 4 == 1) or (i % 4 == 0):
+                noise = np.random.normal(0, NOISE_SCALE)
+                Noise_2[i % 4] = noise
+                step[Negidx] += noise
+            else:
+                step[Negidx] -= Noise_2[i % 4-2]
+        Path.append(step)
+print(Path)
+
+# %%
